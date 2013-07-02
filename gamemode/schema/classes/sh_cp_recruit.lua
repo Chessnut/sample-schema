fa878a8e1cc174fc0d49de367301e28b04c25a45
Chessnut <chessnutist@gmail.com>
2013-07-02 12:06:35 -0700

    Added example of class usage. Soon to change.

---
+++ gamemode/schema/classes/sh_cp_recruit.lua
@@ -0,0 +1,18 @@
+local CLASS = {}
+	CLASS.name = "Metrocop Recruit"
+	CLASS.uniqueID = "cp_rct"
+
+	function CLASS:OnSet(client)
+		client:SetModel("models/Barney.mdl")
+	end
+CLASS_CP_RCT = nut.class.Register(CLASS)
+
+if (SERVER) then
+	nut.char.HookVar("charname", "recruitRanks", function(character)
+		local client = character.player
+
+		if (IsValid(client) and client:Team() == FACTION_CP and string.find(character:GetVar("charname"), "RCT.")) then
+			client:SetCharClass(CLASS_CP_RCT)
+		end
+	end)
+end
