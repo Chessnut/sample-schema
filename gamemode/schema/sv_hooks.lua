a1def83cd69ce8a343e296dab744fb1fc0d8ad13
Chessnut <chessnutist@gmail.com>
2013-07-17 14:35:39 -0700

    Update sv_hooks.lua
    Updated how the syntax is for GetDefaultInv

--- gamemode/schema/sv_hooks.lua
+++ gamemode/schema/sv_hooks.lua
@@ -1,28 +1,11 @@
---[[
-	Purpose: Returns a table that is simply an inventory table. It will be
-	the character's inventory after created.
---]]
-function SCHEMA:GetDefaultInv(client, name, faction)
-	-- CID will be for citizens only.
-	if (faction == FACTION_CITIZEN) then
-		local inventory = {}
-		-- Inventory contains a cid where cid is the item's uniqueID.
-		inventory.cid = {}
-
-		-- First cid item in the inventory.
-		inventory.cid[1] = {
-			-- How many copies of this item it contains.
-			quantity = 1,
-			data = {
-				-- The data contains a random set of digits.
-				Digits = math.random(11111, 99999),
-
-				-- The owner will be set to the character's name.
-				Owner = name
-			}
-		}
-
-		return inventory
+function SCHEMA:GetDefaultInv(inventory, client, data)
+	-- PrintTable(data) to see what information it contains.
+	
+	if (data.faction == FACTION_CITIZEN) then
+		inventory:Add("cid", 1, {
+			Digits = math.random(11111, 99999),
+			Owner = data.charname
+		})
 	end
 end
 
