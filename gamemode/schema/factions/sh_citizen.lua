a0f8ec583a5b0a39adac7a3cc7fb499e54201de4
Brian <chessnutist@gmail.com>
2013-10-28 15:08:25 -0700

    Update sh_citizen.lua

--- gamemode/schema/factions/sh_citizen.lua
+++ gamemode/schema/factions/sh_citizen.lua
@@ -1,8 +1,11 @@
-FACTION_CITIZEN = 1
+-- The 'nice' name of the faction.
+FACTION.name = "Citizen"
+-- A description used in tooltips in various menus.
+FACTION.desc = "The civilian faction that wears blue clothes."
+-- A color to distinguish factions from others, used for stuff such as
+-- name color in OOC chat.
+FACTION.color = Color(20, 150, 15)
 
-local FACTION = {}
-	FACTION.name = "Citizen"
-	FACTION.uniqueID = "civ"
-	FACTION.desc = "The civilian faction that wears blue clothes."
-	FACTION.color = Color(20, 150, 15)
-nut.faction.Register(FACTION_CITIZEN, FACTION)
+-- FACTION.index is defined when the faction is registered and is just a numeric ID.
+-- Here, we create a global variable for easier reference to the ID.
+FACTION_CITIZEN = FACTION.index
