7760ef6c3562928bf37c2e9df61cf7f832ed73c2
Brian <chessnutist@gmail.com>
2013-10-28 15:10:27 -0700

    Update sh_metrocop.lua

--- gamemode/schema/factions/sh_metrocop.lua
+++ gamemode/schema/factions/sh_metrocop.lua
@@ -1,17 +1,24 @@
-FACTION_CP = 2
+-- The 'nice' name of the faction.
+FACTION.name = "Civil Protection"
+-- A description used in tooltips in various menus.
+FACTION.desc = "Citizens who joined the Combine as a policing force."
+-- A color to distinguish factions from others, used for stuff such as
+-- name color in OOC chat.
+FACTION.color = Color(70, 70, 220)
+-- Set the male model choices for character creation.
+FACTION.maleModels = {
+	"models/police.mdl"
+}
+-- Set the female models to be the same as male models.
+FACTION.femaleModels = FACTION.maleModels
+-- Set it so the faction requires a whitelist.
+FACTION.isDefault = false
 
-local FACTION = {}
-	FACTION.name = "Civil Protection"
-	FACTION.uniqueID = "cp"
-	FACTION.desc = "Citizens who joined the Combine as a policing force."
-	FACTION.color = Color(70, 70, 220)
-	FACTION.maleModels = {
-		"models/police.mdl"
-	}
-	FACTION.femaleModels = FACTION.maleModels
-	FACTION.isDefault = false
+-- Return what the name will be set for character creation.
+function FACTION:GetDefaultName()
+	return "CP-RCT."..math.random(11111, 99999)
+end
 
-	function FACTION:GetDefaultName()
-		return "CP-RCT."..math.random(11111, 99999)
-	end
-nut.faction.Register(FACTION_CP, FACTION)
+-- FACTION.index is defined when the faction is registered and is just a numeric ID.
+-- Here, we create a global variable for easier reference to the ID.
+FACTION_CP = FACTION.index
